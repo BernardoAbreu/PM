ec8ecadd5bfea3469d4e5ff5c813010b0555cd94
Bernardo Abreu <bernardoaabreu@gmail.com>
2017-04-01 02:08:04 -0300

    Add Start Position Class

---
+++ TP1/packagePosition/Start.java
@@ -0,0 +1,14 @@
+package packagePosition;
+package packagePosition;
+import packagePlayer.Player;
+
+public class Start extends Position{
+
+	public Start(){
+		super(1);
+	}
+
+	public void play(Player p){
+		p.deposit(500.0);
+	}
+}
