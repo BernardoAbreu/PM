29279a6ae805dadcc27bda6c1b1581278b681159
Bernardo Abreu <bernardoaabreu@gmail.com>
2017-04-02 01:26:13 -0300

    Change ArrayList to array and create getStartPosition method

--- TP1/Board.java
+++ TP1/Board.java
@@ -5,20 +5,31 @@ import java.util.ArrayList;
 public class Board{
 	
 	private int boardSize;
-
-	private ArrayList<Position> positions;
+    private int startPosition;
+	private Position positions[];
 
 	public Board(int boardSize){
 		this.boardSize = boardSize;
-		this.positions = new ArrayList<>(boardSize);
+		this.positions = new Position[boardSize];
 	}
 
 
 	public void addPosition(int boardPosition, Position position){
-		this.positions.add(boardPosition, position);
+	    if (position.getType() == 1){
+	        this.startPosition = boardPosition;
+        }
+		this.positions[boardPosition] = position;
 	}
 
 	public Position getPosition(int boardPosition){
-		return this.positions.get(boardPosition);
+		return this.positions[boardPosition];
+	}
+
+	public int getStartPosition(){
+	    return this.startPosition;
+    }
+
+    public int getBoardSize(){
+		return  this.boardSize;
 	}
 }
