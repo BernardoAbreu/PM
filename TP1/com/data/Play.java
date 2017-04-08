2a825d99832359a6e6520e7b1ea0bf241a971f1c
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-04-08 15:33:48 -0300

    Refactor methods in Monopoly and Play

--- TP1/com/data/Play.java
+++ TP1/com/data/Play.java
@@ -12,19 +12,35 @@ public class Play {
         this.diceValue = diceValue;
     }
 
-    public int getPlayerId() {
-        return playerId;
-    }
+    public void run(Player[] players, Board board){
+        players[this.playerId-1].walk(this.diceValue, board.getBoardSize());
 
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
+        Position current = board.getPosition(players[this.playerId-1].getPosition());
 
-    public int getDiceValue() {
-        return diceValue;
-    }
-
-    public void setDiceValue(int diceValue) {
-        this.diceValue = diceValue;
+        switch (current.getType()){
+            case 1:
+                //Player gets 500 reais from the bank
+                System.out.println("Player: "+ String.valueOf(this.playerId-1) +"  Completed a round trip.");
+                players[this.playerId-1].deposit(500);
+                break;
+            case 2:
+                System.out.println("Player: " + String.valueOf(this.playerId-1) + "  Skipped turn");
+                //Skip turn!!!
+                break;
+            case 3:
+                //Buy Real Estate or pay rent!
+                RealEstate re = (RealEstate) current;
+                System.out.println("Player: " + String.valueOf(this.playerId-1) + "   Wants to buy: " + String.valueOf(re.getRealEstateType()));
+                if(re.getOwner() == null) {
+                    System.out.println("Position belongs to the bank");
+                }
+                else {
+                    System.out.println("Position has an owner");
+                }
+                break;
+            default:
+                System.out.println("ERROR: UNKNOWN POSITION TYPE");
+                System.exit(-1);
+        }
     }
 }
--- TP1/com/game/Monopoly.java
+++ TP1/com/game/Monopoly.java
@@ -3,15 +3,13 @@ package com.game;
 import com.data.*;
 import com.utils.FileUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class Monopoly {
 
     private Board board;
     private Player[] players;
+    private Play[] plays;
 
     public Monopoly(FileUtils handler){
         this.board = null;
@@ -37,58 +35,55 @@ public class Monopoly {
         int nplayers = Integer.parseInt(fields[1]);
         int initialStatement = Integer.parseInt(fields[2]);
 
+        this.plays = new Play[nplays];
         this.players = new Player[nplayers];
 
         int playerId = 0;
         int dice = 0;
+
+        initializePlayers(nplayers, initialStatement);
+
+        Play play;
+        while((play = readPlay(handler,nplays)) != null){
+            play.run(this.players, this.board);
+        }
+//        play = readPlay(handler, nplays);
+
+    }
+
+    private Play readPlay(FileUtils handler, int nplays) {
+        int dice;
+        int playerId;
+
+        String playLine;
+        String[] fields;
+
+        Play play;
+//        for(int i = 0; i < nplays; i++){
+            dice = 0;
+            playLine = handler.getPlayLine();
+
+            if(playLine.equals("DUMP")) return null;
+
+            fields = playLine.split(";");
+
+            if (fields.length!=3){ System.out.println("Wrong number of arguments in play line");}
+
+            playerId = Integer.parseInt(fields[1]);
+            dice = Integer.parseInt(fields[2]);
+            play = new Play(playerId,dice);
+
+
+//        }
+        return play;
+    }
+
+    private void initializePlayers(int nplayers, int initialStatement) {
         for (int k = 0; k < nplayers; k++) {
             this.players[k] = new Player(k);
             this.players[k].deposit(initialStatement);
         }
-
-        for(int i = 0; i < nplays; i++){
-            dice = 0;
-            playLine = handler.getPlayLine();
-//            System.out.println(playLine);
-
-            if(playLine.equals("DUMP")) break;
-
-            fields = playLine.split(";");
-
-            if (fields.length!=3){ System.out.println("Wrong number of arguments in runGame number: " + String.valueOf(i));}
-
-            playerId = Integer.parseInt(fields[1]);
-            dice = Integer.parseInt(fields[2]);
-
-            runPlay(playerId,dice);
-
-        }
-
     }
 
-    private void runPlay(int playerId, int dice){
-        this.players[playerId-1].walk(dice, this.board.getBoardSize());
 
-        Position current = board.getPosition(this.players[playerId-1].getPosition());
-
-        switch (current.getType()){
-            case 1:
-                //Player gets 500 reais from the bank
-                System.out.println("Player: "+ String.valueOf(playerId-1) +"  Completed a round trip.");
-                this.players[playerId-1].deposit(500);
-                break;
-            case 2:
-                System.out.println("Player: " + String.valueOf(playerId-1) + "  Skipped turn");
-                //Skip turn!!!
-                break;
-            case 3:
-                //Buy Real Estate or pay rent!
-                RealEstate re = (RealEstate) current;
-                System.out.println("Player: " + String.valueOf(playerId-1) + "   Wants to buy: " + String.valueOf(re.getRealEstateType()));
-                break;
-            default:
-                    System.out.println("ERROR: UNKNOWN POSITION TYPE");
-                    System.exit(-1);
-        }
-    }
 }
