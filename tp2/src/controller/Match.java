b57829a38a0f9ca51c1335ad9b5e79f023f2c460
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-05-06 15:47:48 -0300

    Build deck sets card values, cards are comparable

--- tp2/src/controller/Match.java
+++ tp2/src/controller/Match.java
@@ -1,6 +1,11 @@
 package controller;
 
+import model.Card;
+import model.Deck;
+import model.Suits;
 import view.Display;
+
+import java.util.SplittableRandom;
 
 /**
  * Created by math on 5/5/17.
@@ -9,5 +14,28 @@ public class Match {
 
     public void run(Display d){
         d.printString("IN match, register display observer: ");
+        Deck deck = new Deck();
+        deck.printDeck();
+
+        System.out.println("================= SHUFFLE =====================");
+        deck.shuffleDeck();
+//        deck.printDeck();
+        System.out.println("=================== GETFIRST ===================");
+        Card c = deck.getFirstCard();
+        System.out.println("c suit:  " + String.valueOf(c.getSuit()) + "  c value: " + String.valueOf(c.getValue()));
+//        deck.printDeck();
+
+        Card c2 = deck.getFirstCard();
+        System.out.println("c2 suit:  " + String.valueOf(c2.getSuit()) + "  c2 value: " + String.valueOf(c2.getValue()));
+        int result = c.compareTo(c2);
+        if(result == 0){
+            System.out.println("igual");
+        }else if(result == -1){
+            System.out.println("menor");
+        }else if(result ==1){
+            System.out.println("maior");
+        }else{
+            System.out.println("ERROR");
+        }
     }
 }
--- tp2/src/model/Deck.java
+++ tp2/src/model/Deck.java
@@ -1,12 +1,79 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Math on 5/6/2017.
  */
 public class Deck {
-    private Card[] cards;
+    private List<Card> cards;
 
     public Deck(){
-        this.cards = new Card[40];
+        this.cards = new ArrayList<Card>();
+        //MANILHAS
+        this.cards.add(new Card(Suits.CLUBS, '4', 14));
+        this.cards.add(new Card(Suits.HEARTS, '7', 13));
+        this.cards.add(new Card(Suits.SPADES, '1', 12));
+        this.cards.add(new Card(Suits.DIAMONDS, '7', 11));
+        //FIRST CARDS
+        this.cards.add(new Card(Suits.CLUBS, '3', 10));
+        this.cards.add(new Card(Suits.HEARTS, '3', 10));
+        this.cards.add(new Card(Suits.SPADES, '3', 10));
+        this.cards.add(new Card(Suits.DIAMONDS, '3', 10));
+
+        this.cards.add(new Card(Suits.CLUBS, '2', 9));
+        this.cards.add(new Card(Suits.HEARTS, '2', 9));
+        this.cards.add(new Card(Suits.SPADES, '2', 9));
+        this.cards.add(new Card(Suits.DIAMONDS, '2', 9));
+
+        this.cards.add(new Card(Suits.CLUBS, '1', 8));
+        this.cards.add(new Card(Suits.HEARTS, '1', 8));
+        this.cards.add(new Card(Suits.DIAMONDS, '1', 8));
+        //King
+        this.cards.add(new Card(Suits.CLUBS, 'K', 7));
+        this.cards.add(new Card(Suits.HEARTS, 'K', 7));
+        this.cards.add(new Card(Suits.SPADES, 'K', 7));
+        this.cards.add(new Card(Suits.DIAMONDS, 'K', 7));
+        //Knight
+        this.cards.add(new Card(Suits.CLUBS, 'J', 6));
+        this.cards.add(new Card(Suits.HEARTS, 'J', 6));
+        this.cards.add(new Card(Suits.SPADES, 'J', 6));
+        this.cards.add(new Card(Suits.DIAMONDS, 'J', 6));
+        //Queen
+        this.cards.add(new Card(Suits.CLUBS, 'Q', 5));
+        this.cards.add(new Card(Suits.HEARTS, 'Q', 5));
+        this.cards.add(new Card(Suits.SPADES, 'Q', 5));
+        this.cards.add(new Card(Suits.DIAMONDS, 'Q', 5));
+
+        this.cards.add(new Card(Suits.CLUBS, '7',4));
+        this.cards.add(new Card(Suits.SPADES, '7',4));
+        this.cards.add(new Card(Suits.CLUBS, '6',3));
+        this.cards.add(new Card(Suits.SPADES, '6',3));
+        this.cards.add(new Card(Suits.HEARTS, '6',3));
+        this.cards.add(new Card(Suits.DIAMONDS, '6',3));
+        this.cards.add(new Card(Suits.CLUBS, '5',2));
+        this.cards.add(new Card(Suits.HEARTS, '5',2));
+        this.cards.add(new Card(Suits.SPADES, '5',2));
+        this.cards.add(new Card(Suits.DIAMONDS, '5',2));
+        this.cards.add(new Card(Suits.HEARTS, '4',1));
+        this.cards.add(new Card(Suits.SPADES, '4',1));
+        this.cards.add(new Card(Suits.DIAMONDS, '4',1));
+    }
+
+    public Card getFirstCard(){
+        Card c = this.cards.get(0);
+        this.cards.remove(c);
+        return c;
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(this.cards);
+    }
+
+    public void printDeck(){
+        this.cards.forEach(x-> System.out.println("Suit:  " + String.valueOf(x.getSuit()) + "  Value:  " + String.valueOf(x.getValue())));
     }
 }
